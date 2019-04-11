23b244a4048d43340db92ebf482b24b165688a1d
yoyoyo <jiang20nan@gmail.com>
2019-04-11 16:01:07 +0800

    Add files via upload

--- js/ship.js
+++ js/ship.js
@@ -7,25 +7,25 @@ function Ship(){
 	this.showFlame = false;
 }
 
-Ship.prototype.draw = function(content){
-	content.save();
-	content.translate(this.x, this.y);
-	content.rotate(this.rotation);
-	content.lineWidth = 2;
-	content.strokeStyle = '#ffffff';
-	content.beginPath();
-	content.moveTo(10, 0);
-	content.lineTo(-10, 10);
-	content.lineTo(-5, 0);
-	content.lineTo(-10, -10);
-	content.lineTo(10, 0);
-	content.stroke();
+Ship.prototype.draw = function(context){
+	context.save();
+	context.translate(this.x, this.y);
+	context.rotate(this.rotation);
+	context.lineWidth = 1;
+	context.strokeStyle = '#ffffff';
+	context.beginPath();
+	context.moveTo(10, 0);
+	context.lineTo(-10, 10);
+	context.lineTo(-5, 0);
+	context.lineTo(-10, -10);
+	context.lineTo(10, 0);
+	context.stroke();
 	if(this.showFlame){
-		content.beginPath();
-		content.moveTo(-7.5, -5);
-		content.lineTo(-15, 0);
-		content.lineTo(-7.5, 5);
-		content.stroke();
+		context.beginPath();
+		context.moveTo(-7.5, -5);
+		context.lineTo(-15, 0);
+		context.lineTo(-7.5, 5);
+		context.stroke();
 	}
-	content.restore();
+	context.restore();
 }
